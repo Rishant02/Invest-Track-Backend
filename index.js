293898e210c33283e65b4352aa90dbc162edbfa1
Rishant Mastwal <rishantmastwal@gmail.com>
2024-05-29 14:58:18 +0530

    helmet config changes

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ connectDB();
 const app = express();
 
 // required middlewares
-app.use(helmet({ contentSecurityPolicy: false })); //TODO: Will remove this after changing user schema
+app.use(helmet());
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "credentialless" }));
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    originAgentCluster: true,
+  })
+);
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https:", "data:", "blob:"],
+    },
+  })
+);
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
